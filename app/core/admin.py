d2bdd45f8f79e0900499561d499e650e76210e3d
ramnitesh <ramnitesh.saran@gmail.com>
2024-05-11 16:43:50 +0530

    lint resolved

--- app/core/admin.py
+++ app/core/admin.py
@@ -24,7 +24,9 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'name', 'is_active', 'is_staff', 'is_superuser')
+            'fields': ('email', 'password1', 'password2',
+                       'name', 'is_active', 'is_staff',
+                       'is_superuser')
         }),
     )
 
